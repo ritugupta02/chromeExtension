7a5fea48ed4d8f79bf2aaa930ac60e9373792447
ritugupta02 <85019207+ritugupta02@users.noreply.github.com>
2021-10-02 14:55:35 +0530

    Added

--- chromeExtension/quote.js
+++ chromeExtension/quote.js
@@ -1,10 +1,4 @@
 
-// async function getlink() {
-//     const res = await fetch('https://meme-api.herokuapp.com/gimme')
-//     const data = await res.json()
-//     return data;
-// }
-
 const para = document.querySelector('p');
 fetch('https://api.quotable.io/random')
     .then(res => res.json())
